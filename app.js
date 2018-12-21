16bacda05cab56dbb695fb6428531bf48cf6780b
cheetax <dmitriy.grebenev@gmail.com>
2018-12-21 08:16:52 +0300

    new

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var config = require('./config');
-
 var base = require('./routes/base');
-
-var express = require('express')
+var express = require('express');
+var cors = require('cors');
 
 var app = express();
+
+var corsOptions = {
+    origin: (origin, callback) => {
+        callback(null, origin)
+    },
+    credentials: true
+}
+
+app.use(cors(corsOptions));
 
 app.use('/base', base)
 
@@ -13,5 +21,3 @@ app.get('/', function (req, res) {
 })
 
 app.listen(4000)
-
-
