920a3b2f21f319aed3601e41432a198f3da2a137
cheetax <dmitriy.grebenev@gmail.com>
2018-12-24 17:10:12 +0300

    new

--- services/base.js
+++ services/base.js
@@ -25,23 +25,16 @@ groupBy = (list, keyGroup, keyAggregate) => {
             collection.push(keyAggregate(item));
         }
     });
-    //var result = []
-    //map.forEach((item, key) => result.push({company: key, department: collapseBy(item, item => item) }))
     return map;
 }
 
-
-
 initialDate = async () => {
     users = await getUsersAD();
-    //usersMap = groupBy(users, user => groupBy())
     company = collapseBy(users, user => user.company)
     companyMap = groupBy(users, user => user.company, user => user)    
     departmentMap = groupBy(users, user => user.company, user => user.department)
-    //var result = []
     departmentMap.forEach((item, key) => department.push({company: key, department: collapseBy(item, item => item) }))
     companyMap.forEach((item, key) => usersMap.set(key, groupBy(item, user => user.department, user => user)))
-    //console.log(department)
 }
 
 initialDate();
